6a9825efb81bda23fe9a620b4e8a195fbbc7ecdc
Mac <mac@Macs-MacBook-Pro.local>
2016-12-25 15:37:49 -0500

    initial commit

--- TipMe/ViewController.swift
+++ TipMe/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var noOfPeopleField: UITextField!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var perPersonLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +29,32 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    private var total:Double = 0
+    
+    @IBAction func calculateTip(_ sender: Any) {
+    
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func perPersonTips(_ sender: Any) {
+        let noPeople = Double(noOfPeopleField.text!) ?? 0
+        let perPerson = total / noPeople
+        perPersonLabel.text = String(format: "$%.2f", perPerson)
+        
+    }
+    
 }
 
